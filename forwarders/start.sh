48d9fe61dbffc010e7ebb05ac656e997c633f981
Corentin Dupont <corentin.dupont@gmail.com>
2020-11-14 22:30:23 +0100

    sort forwarders by folders and symlink conf files

--- forwarders/start.sh
+++ forwarders/start.sh
@@ -2,46 +2,60 @@
 # This file initiates the LoRa packet forwarders in a fail-switch manner in order to 
 # match with the hardware installed on the pi
 
-# function definitions
+echo -e "Setting the Gateway ID..."
+GWID=$(curl -s http://wazigate-edge/device/id | tr -d '"')
+echo -e "Gateway ID is: ${GWID}"
 
-# reset the concentrator via GPIO pins
-function reset_gpio {
+cfgFiles=("/root/multi_chan_pkt_fwd/global_conf.json" "/root/multi_chan_pkt_fwd/local_conf.json" "/root/single_chan_pkt_fwd/global_conf.json" "/root/single_chan_pkt_fwd/local_conf.json")
 
-  echo -e "Restarting the CE0 pin..."
-  CSPIN=8
-  echo "$CSPIN" > /sys/class/gpio/export
-  sleep 1
-  echo "out" > /sys/class/gpio/gpio$CSPIN/direction
-  echo "0" > /sys/class/gpio/gpio$CSPIN/value
-  sleep 1
-  echo "1" > /sys/class/gpio/gpio$CSPIN/value
-  echo -e "Done\n"
-  sleep 1
-  
-  echo -e "Restarting the Concentrator..."
-  RSTPIN=17
-  echo "$RSTPIN" > /sys/class/gpio/export
-  sleep 1
-  echo "out" > /sys/class/gpio/gpio$RSTPIN/direction
-  echo "1" > /sys/class/gpio/gpio$RSTPIN/value
-  sleep 1
-  echo "0" > /sys/class/gpio/gpio$RSTPIN/value
-  sleep 1
-}
+#mkdir -p tmp
+#for f in "${cfgFiles[@]}"
+#do
+#    rm -f ./tmp/*
+#    cp $f ./tmp/test.json
+#    sed -i 's/\(^\s*"gateway_ID":\s*"\).*"\s*\(,\?\).*$/\1'${GWID}'"\2/' ./tmp/test.json
+#    cp -f ./tmp/test.json $f
+#    echo -e "[ $f ]" "Done"
+#done
 
-# Start the forwarder
-# parameter 1: location of the forwarder executable
-# parameter 2: location of the config file (global_conf.json)
-function start_forwarder {
-    cd $(dirname "$1")
-    # Linking global_conf.json
-    ln -s $2
-    ls
-    # Create the local_conf.json with our gateway ID
-    echo "{\"gateway_conf\": {\"gateway_ID\": \"${GWID}\"}}" >> local_conf.json
-    # launch the concentrator
-    ./$(basename "$1")
-}
+pwd
+find
+
+#---------------------------------------------#
+
+
+# LoRaWAN Multi Channel
+echo -e "\n\n============================\n"
+echo -e "Initiating the multi-channel Lora packet forwarder..."
+echo -e "\n============================\n\n"
+
+echo -e "Restarting the CE0 pin..."
+CSPIN=8
+echo "$CSPIN" > /sys/class/gpio/export
+sleep 1
+echo "out" > /sys/class/gpio/gpio$CSPIN/direction
+echo "0" > /sys/class/gpio/gpio$CSPIN/value
+sleep 1
+echo "1" > /sys/class/gpio/gpio$CSPIN/value
+echo -e "Done\n"
+sleep 1
+
+echo -e "Restarting the Concentrator..."
+RSTPIN=17
+echo "$RSTPIN" > /sys/class/gpio/export
+sleep 1
+echo "out" > /sys/class/gpio/gpio$RSTPIN/direction
+echo "1" > /sys/class/gpio/gpio$RSTPIN/value
+sleep 1
+echo "0" > /sys/class/gpio/gpio$RSTPIN/value
+echo -e "Done\nLaunching the forwarder..."
+sleep 2
+
+cd spi_multi_chan/
+ln -s ~/conf/multi_chan_pkt_fwd/global_conf.json
+ln -s ~/conf/multi_chan_pkt_fwd/local_conf.json
+ls -la
+./lora_pkt_fwd
 
 
 GWID=$(curl -s http://wazigate-edge/device/id | tr -d '"')
@@ -64,9 +78,9 @@ echo "1" > /sys/class/gpio/gpio$RSTPIN/value
 echo -e "Done\nLaunching the forwarder..."
 sleep 1
 
-echo -e "Initiating the single-channel Lora packet forwarder..."
-reset_gpio
-start_forwarder ~/single_chan/lora_pkt_fwd ~/conf/single_chan_pkt_fwd/global_conf.json
+cd single_chan/
+ln -s ~/conf/single_chan_pkt_fwd/global_conf.json
+./lora_pkt_fwd
 
 # In future we might need to have some sort of configuration 
 # for the selection or just remove the latest option
@@ -85,7 +99,9 @@ echo -e "\n\n============================\n"
 echo -e "Initiating the HT-M01 Lora packet forwarder..."
 echo -e "\n============================\n\n"
 
-cd /root/picoGW_pkt_fwd/
+cd usb_multi_chan/
+ln -s ~/conf/multi_chan_pkt_fwd/global_conf.json
+ln -s ~/conf/multi_chan_pkt_fwd/local_conf.json
 ./lora_pkt_fwd
 
 echo -e "All forwarders failed, exiting." 
