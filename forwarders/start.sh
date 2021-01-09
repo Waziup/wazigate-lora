f15788a3b9427d3b7854aef73b6fd3d4909e96fe
mojtaba-esk <mojtaba.esk@gmail.com>
2021-01-09 19:50:24 +0100

    Forwarder selective enabler added

--- forwarders/start.sh
+++ forwarders/start.sh
@@ -44,52 +44,79 @@ function start_forwarder {
     ./$(basename "$1")
 }
 
-
 GWID=$(curl -s http://wazigate-edge/device/id | tr -d '"')
 echo -e "Gateway ID is: ${GWID}"
 
-# Trying all concentrators, one at a time
+# Trying all enabled concentrators, one at a time
 
-echo -e "\n============================\n"
-echo -e "Initiating the SPI multi-channel Lora packet forwarder..."
-echo -e "\n============================\n\n"
-~/spi_multi_chan/reset_lgw.sh start 8
-reset_gpio
-start_forwarder ~/spi_multi_chan/lora_pkt_fwd ~/conf/multi_chan_pkt_fwd/global_conf.json
-~/spi_multi_chan/reset_lgw.sh stop 8
+VAR_ENABLE_MULTI_SPI=${ENABLE_MULTI_SPI:-0}
+if [ "$VAR_ENABLE_MULTI_SPI" == "1" ]; then
 
-echo -e "\n============================\n"
-echo -e "Initiating the single-channel Lora packet forwarder..."
-echo -e "\n============================\n\n"
+  echo -e "\n============================\n"
+  echo -e "Initiating the SPI multi-channel Lora packet forwarder..."
+  echo -e "\n============================\n\n"
 
-#reset_gpio
-echo -e "Restarting the CE0 pin..."
-CSPIN=8
-echo "$CSPIN" > /sys/class/gpio/export
-sleep 1
-echo "out" > /sys/class/gpio/gpio$CSPIN/direction
-echo "0" > /sys/class/gpio/gpio$CSPIN/value
-sleep 1
-echo "1" > /sys/class/gpio/gpio$CSPIN/value
-echo -e "Done\n"
-sleep 1
+  ~/spi_multi_chan/reset_lgw.sh start 8
+  reset_gpio
+  start_forwarder ~/spi_multi_chan/lora_pkt_fwd ~/conf/multi_chan_pkt_fwd/global_conf.json
+  ~/spi_multi_chan/reset_lgw.sh stop 8
 
-echo -e "Restarting the RFM95x Module..."
-RSTPIN=17
-echo "$RSTPIN" > /sys/class/gpio/export
-sleep 1
-echo "out" > /sys/class/gpio/gpio$RSTPIN/direction
-echo "0" > /sys/class/gpio/gpio$RSTPIN/value
-sleep 0.0001
-echo "1" > /sys/class/gpio/gpio$RSTPIN/value
-echo -e "Done\nLaunching the forwarder..."
-sleep 1
-start_forwarder ~/single_chan/lora_pkt_fwd ~/conf/single_chan_pkt_fwd/global_conf.json
+fi
 
-echo -e "\n============================\n"
-echo -e "Initiating the USB multi-channel Lora packet forwarder..."
-echo -e "\n============================\n\n"
-start_forwarder ~/usb_multi_chan/lora_pkt_fwd ~/conf/multi_chan_pkt_fwd/global_conf.json
+#---------------------------------------#
 
-echo -e "All forwarders failed, exiting." 
+VAR_ENABLE_SINGLE_SPI=${ENABLE_SINGLE_SPI:-0}
+if [ "$VAR_ENABLE_SINGLE_SPI" == "1" ]; then
+
+  echo -e "\n============================\n"
+  echo -e "Initiating the single-channel Lora packet forwarder..."
+  echo -e "\n============================\n\n"
+
+  #reset_gpio
+  echo -e "Restarting the CE0 pin..."
+  CSPIN=8
+  echo "$CSPIN" > /sys/class/gpio/export
+  sleep 1
+  echo "out" > /sys/class/gpio/gpio$CSPIN/direction
+  echo "0" > /sys/class/gpio/gpio$CSPIN/value
+  sleep 1
+  echo "1" > /sys/class/gpio/gpio$CSPIN/value
+  echo -e "Done\n"
+  sleep 1
+
+  echo -e "Restarting the RFM95x Module..."
+  RSTPIN=17
+  echo "$RSTPIN" > /sys/class/gpio/export
+  sleep 1
+  echo "out" > /sys/class/gpio/gpio$RSTPIN/direction
+  echo "0" > /sys/class/gpio/gpio$RSTPIN/value
+  sleep 0.0001
+  echo "1" > /sys/class/gpio/gpio$RSTPIN/value
+  echo -e "Done\nLaunching the forwarder..."
+  sleep 1
+  start_forwarder ~/single_chan/lora_pkt_fwd ~/conf/single_chan_pkt_fwd/global_conf.json
+
+fi
+
+#---------------------------------------#
+
+VAR_ENABLE_MULTI_USB=${ENABLE_MULTI_USB:-0}
+if [ "$VAR_ENABLE_MULTI_USB" == "1" ]; then
+
+  echo -e "\n============================\n"
+  echo -e "Initiating the USB multi-channel Lora packet forwarder..."
+  echo -e "\n============================\n\n"
+  start_forwarder ~/usb_multi_chan/lora_pkt_fwd ~/conf/multi_chan_pkt_fwd/global_conf.json
+
+fi
+
+#---------------------------------------#
+
+echo -e "\n............................\n"
+echo -e "Enabled Forwarders: "
+if [ "$VAR_ENABLE_MULTI_SPI" == "1" ]; then echo "\n\t-\t MULTI_SPI"; fi
+if [ "$VAR_ENABLE_MULTI_USB" == "1" ]; then echo "\n\t-\t MULTI_USB"; fi
+if [ "$VAR_ENABLE_SINGLE_SPI" == "1" ]; then echo "\n\t-\t SINGLE_SPI"; fi
+
+echo -e "\nNo forwarders could make it, exiting." 
 exit 2
